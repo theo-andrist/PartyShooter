f6be73de50444f84ca7e8b3d5b4dfb1dc72af23b
GreySilverWolf <kanno.clash6@gmail.com>
2019-11-28 09:33:32 +0100

    Magnum + Mine + Connect Scene
    Not harmfully

---
+++ PartyShooter_2018/Assets/Scripts/MagnumPistol.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnumPistol : MonoBehaviour
+{
+    public GameObject Bullet;
+    public GameObject BulletSpawn;
+
+    private GameObject GunPivot;
+
+    void Awake()
+    {
+        GunPivot = transform.parent.gameObject;
+    }
+
+    public void Shoot()
+    {
+        gameObject.SetActive(true);
+        Instantiate(Bullet, BulletSpawn.transform.position, GunPivot.transform.rotation);
+    }
+}
---
+++ PartyShooter_2018/Assets/Scripts/PistolBullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistolBullet : MonoBehaviour
+{
+    private GameObject weapon;
+    public GameObject Weapon
+    {
+        set
+        {
+            weapon = value;
+        }
+    }
+    private float speed;
+
+    private float damage;
+
+    void Start()
+    {
+        speed = weapon.GetComponent<WeaponManager>().bulletSpeed;
+        damage = weapon.GetComponent<WeaponManager>().bulletDamage;
+    }
+    private void FixedUpdate()
+    {
+        transform.position += -transform.up * speed * Time.deltaTime;
+
+        Destroy(gameObject, 5f);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().TakeRockhitDamage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
